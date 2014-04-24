472b04fdc753e82730984b87713bf193b3d38505
lazyroot <cuipeng.star@gmail.com>
2014-04-24 10:22:27 +0800

    常量工厂

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -2,6 +2,10 @@ package core;
 
 public class Constants {
 	
+	//验证码
 	public final static String RANDOM_CODE_KEY = "1";
+	
+	//Email配置文件
+	public static final String EMAIL_PROP_FILE = "mail.properties";
 
 }
