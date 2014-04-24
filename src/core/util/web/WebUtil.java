78e32ab635a608e742cb061c5903803cd0ec78a3
lazyroot <cuipeng.star@gmail.com>
2014-04-24 10:23:24 +0800

    Web工具类，添加IP地址获取

--- src/core/util/web/WebUtil.java
+++ src/core/util/web/WebUtil.java
@@ -271,5 +271,27 @@ public class WebUtil {
 		cookie.setPath(COOKIE_PATH);
 		response.addCookie(cookie);
 	}
+	
+	/**
+	 * @title getReaIpAddr
+	 * @description 获取真实的请求IP地址
+	 * @param @param request
+	 * @param @return
+	 * @return String
+	 * @date 2014年4月23日
+	 */
+	public static String getReaIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
 
 }
